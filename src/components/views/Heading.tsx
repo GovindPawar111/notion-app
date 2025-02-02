893620eaaceae2bb2e0907eaa66beb677503abb5
Govind Pawar <pawargovind512@gmail.com>
2025-02-02 20:05:16 +0530

    added the google font and updated the heading component

--- src/components/views/Heading.tsx
+++ src/components/views/Heading.tsx
@@ -2,8 +2,15 @@
 
 export const Heading = () => {
 	return (
-		<div>
-			<header>di</header>
+		<div className="max-w-3xl space-y-4">
+			<h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
+				Your Ideas, Documents & Plans, Unified. Welcome to{' '}
+				<span className="underline">Notion</span>
+			</h1>
+			<h3 className="text-base sm:text-xl md:text-2xl font-medium">
+				Notion is connected workspace where <br />
+				better, faster work happens.
+			</h3>
 		</div>
 	)
 }
