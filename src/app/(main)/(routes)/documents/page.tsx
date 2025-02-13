0c37231a821e912bd1d08d4dd65cfc5085a402f8
Govind Pawar <pawargovind512@gmail.com>
2025-02-13 21:11:58 +0530

    Completed the Log out functionality

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -1,7 +1,35 @@
+'use client'
+
+import Image from 'next/image'
+import { useUser } from '@clerk/clerk-react'
+import { Button } from '@/components/ui/button'
+import { PlusCircle } from 'lucide-react'
+
 export const DocumentsPage = (): JSX.Element => {
+	const { user } = useUser()
 	return (
-		<div>
-			<h1>Documents</h1>
+		<div className="h-full flex flex-col items-center justify-center space-y-4">
+			<Image
+				src="/thinking.webp"
+				alt="thinking"
+				width="300"
+				height="300"
+				className="dark:hidden"
+			/>
+			<Image
+				src="/thinking-dark.webp"
+				alt="thinking"
+				width="300"
+				height="300"
+				className="hidden dark:block"
+			/>
+			<h2 className="text-lg font-medium">
+				Welcome to {user?.firstName}&apos;s Notion
+			</h2>
+			<Button>
+				<PlusCircle className="h-4 w-4 mr-2" />
+				Create a note
+			</Button>
 		</div>
 	)
 }
