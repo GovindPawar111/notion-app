b907b288cfea42532a300db57dd34f035273ce3c
Govind Pawar <pawargovind512@gmail.com>
2025-07-20 20:16:19 +0530

    Completed the documents expand and nested document creation functionality

--- src/components/views/main/Item.tsx
+++ src/components/views/main/Item.tsx
@@ -1,9 +1,13 @@
 'use client'
 
 import { Skeleton } from '@/components/ui/skeleton'
+import { api } from '@/convex/_generated/api'
 import { Id } from '@/convex/_generated/dataModel'
 import { cn } from '@/lib/utils'
-import { ChevronDown, ChevronRight, LucideIcon } from 'lucide-react'
+import { useMutation } from 'convex/react'
+import { ChevronDown, ChevronRight, LucideIcon, Plus } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { toast } from 'sonner'
 
 interface ItemProps {
 	onClick: () => void
@@ -30,7 +34,36 @@ const Item = ({
 	level = 0,
 	onExpand,
 }: ItemProps): JSX.Element => {
+	const create = useMutation(api.documents.create)
+	const router = useRouter()
 	const ChevronIcon = expanded ? ChevronDown : ChevronRight
+
+	const handleExpand = (
+		event: React.MouseEvent<HTMLDivElement, MouseEvent>,
+	) => {
+		event.stopPropagation()
+		onExpand?.()
+	}
+
+	const onCreate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+		event.stopPropagation()
+		if (!id) return
+
+		const promise = create({ title: 'Untitled', parentDocument: id }).then(
+			(documentId) => {
+				if (!expanded) {
+					onExpand?.()
+				}
+				// router.push(`/documents/${documentId}`)
+			},
+		)
+
+		toast.promise(promise, {
+			loading: 'Creating a new note...',
+			success: 'New note created!',
+			error: 'Failed to create a new note.',
+		})
+	}
 
 	return (
 		<div
@@ -46,9 +79,7 @@ const Item = ({
 				<div
 					role="button"
 					className="h-full rounded-sm hover:bg-neutral-300 dark:bg-neutral-600 mr-1"
-					onClick={() => {
-						return
-					}}
+					onClick={handleExpand}
 				>
 					<ChevronIcon className="h-4 w-4 shrink-0 text-muted-foreground/50" />
 				</div>
@@ -67,6 +98,18 @@ const Item = ({
 					<span className="text-xs">⌘</span>
 					<span className="text-sm">K</span>
 				</kbd>
+			)}
+
+			{!!id && (
+				<div className="ml-auto flex items-center gap-x-2">
+					<div
+						role="button"
+						onClick={onCreate}
+						className="opacity-0 group-hover:opacity-100 h-full ml-auto rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600"
+					>
+						<Plus className="h-4 w-4 text-muted-foreground" />
+					</div>
+				</div>
 			)}
 		</div>
 	)
